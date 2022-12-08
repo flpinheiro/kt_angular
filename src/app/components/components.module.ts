e11a5f5738af6fc1008aa511b4d000de60c83621
flpinheiro <flpinheiro@gmail.com>
2022-12-08 15:55:13 -0300

    initial commit

---
+++ src/app/components/components.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { EmsButtonComponent } from './ems-button/ems-button.component';
+
+
+
+@NgModule({
+  declarations: [
+    EmsButtonComponent
+  ],
+  imports: [
+    CommonModule
+  ]
+})
+export class ComponentsModule { }
